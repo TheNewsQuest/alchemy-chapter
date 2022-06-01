d360f516a8d39dbfcb5df6cddddd49211c623520
bietdoikiem <minhlucky2408@gmail.com>
2022-06-01 10:29:43 +0700

    feat: enhance Build Scripts

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ FROM python:3.8
 RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 ENV PATH="/root/.cargo/bin:${PATH}"
 
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
 WORKDIR /usr/app/src
 
 # Install app's dependencies
@@ -13,4 +16,4 @@ RUN python -m nltk.downloader punkt
 COPY . .
 
 # Run Gunicorn HTTP Server
-CMD ["sh", "-c", "gunicorn --chdir /usr/app/src api_gateway:app -w ${APP_WORKERS} --threads ${APP_THREADS} -b=0.0.0.0:${APP_PORT}"]
+CMD ["sh", "-c", "gunicorn --chdir /usr/app/src api_gateway:app --worker-class=${GUNICORN_WORKER_CLASS} --worker-connections=${GUNICORN_WORKER_CONNECTIONS=} -w ${GUNICORN_WORKERS} --threads ${GUNICORN_THREADS} --limit-request-line 0 -b=0.0.0.0:${APP_PORT} --timeout 60"]
--- api_gateway.py
+++ api_gateway.py
@@ -4,20 +4,21 @@ import json
 from app.mcq_generation import MCQGenerator
 
 app = Flask(__name__)
+MCQ_Generator = MCQGenerator(is_verbose=True)
 
 @app.route("/questgen", methods=["POST"])
 def questgen():
-    payload = json.loads(request.data)
-    text = payload['content']
-    count = 5 # if payload['count'] == '' else int(payload['count'])
-    
-    questions = MCQ_Generator.generate_mcq_questions(text, count)
-    result = list(map(lambda x: x.__dict__, questions))
+  payload = json.loads(request.data)
+  text = payload['content']
+  count = 10
+  
+  questions = MCQ_Generator.generate_mcq_questions(text, count)
+  result = list(map(lambda x: x.__dict__, questions))
 
-    return jsonify(result)
+  return jsonify(result)
 
 if __name__ == '__main__':
-    MCQ_Generator = MCQGenerator(is_verbose=True)
-    app.run(debug=True, port=9090)
+  # MCQ_Generator = MCQGenerator(is_verbose=True)
+  app.run(debug=True, port=9090)
 
 
